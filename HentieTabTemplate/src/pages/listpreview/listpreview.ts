fa4502f7e2830759adf93854703d9866c1c65c97
Hentie <32867407+HentiePaxton@users.noreply.github.com>
2019-01-22 21:37:08 +0200

    Initial commit

---
+++ HentieTabTemplate/src/pages/listpreview/listpreview.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'listpreview.html'
+})
+export class ListpreviewPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
